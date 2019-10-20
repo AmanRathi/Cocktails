e68a44c2f47ff5abe3f49c7cf1ccde233b3dd7d7
Aman <aman@amans-air.attlocal.net>
2019-10-20 02:28:17 -0500

    Added all relevant files

---
+++ script.js
@@ -0,0 +1,113 @@
+let userInput = document.getElementById('user-input')
+let main = document.getElementById('main')
+let http = new XMLHttpRequest()
+let res = null;
+let url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+let newAwesomplete = new Awesomplete(userInput, {autoFirst: true});
+let cNames = null;
+let cID = null;
+
+let cocktail = {
+    id: "",
+    name: "",
+    alcoholic: "",
+    pic: "",
+    ingridients: {
+        pics: [],
+        name: [],
+        qty: []
+    },
+    directions: []
+}
+
+let fecthData = () =>{
+    http.open('GET', url + userInput.value, true)
+    http.responseType = 'json'
+    
+    http.onload = () => {
+        res = http.response;
+        if(res.drinks == null){
+            return;
+        }
+        cNames = res.drinks.map((value, index, arr) => {
+            return value.strDrink;
+        })
+        newAwesomplete.list = cNames;
+        cID = res.drinks.map((value, index, arr) => {
+            return value.idDrink;
+        })
+    }
+
+    // http.onerror = () => {
+    //     //Error handing goes here
+    // }
+
+    // http.onprogress = () => {
+    //     //While req is under progress this methid will execute
+    // }
+
+    http.send()
+}
+
+userInput.addEventListener('input', fecthData)
+userInput.addEventListener('awesomplete-select', obj => {
+    //obj.text
+    for(let i=0; i<res.drinks.length; i++){
+        if(res.drinks[i].strDrink == obj.text){
+            //console.log(res.drinks[i]);
+            temp = res.drinks[i]
+            cocktail.id = temp.idDrink
+            cocktail.name = temp.strDrink
+            cocktail.alcoholic = temp.strAlcoholic
+            cocktail.pic = temp.strDrinkThumb
+            cocktail.directions = temp.strInstructions.split('.')
+            for(let i = 1; 1<=15; i++){
+                if(temp['strIngredient' + i] == null){
+                    break;
+                }
+                cocktail.ingridients.name[i-1] = temp['strIngredient' + i]
+                cocktail.ingridients.pics[i-1] = 'https://www.thecocktaildb.com/images/ingredients/' + temp['strIngredient' + i] + '-Medium.png'
+                cocktail.ingridients.qty[i-1] = temp['strMeasure' + i]
+            }
+            console.log(cocktail);
+            
+            //main.innerHTML = JSON.stringify(cocktail, null, 2)
+            break;
+        }
+    }
+})
+
+
+
+
+
+
+
+// let cocktail2 = {
+//     id: "12345",
+//     name: "California Lemonade",
+//     alcoholic: "Alcoholic",
+//     pic: "http://google.com/image.jpg",
+//     ingridients: {
+//         pics: [
+//             'link1',
+//             'link2',
+//             'link3'
+//         ],
+//         name: [
+//             'name1',
+//             'name2',
+//             'name3'
+//         ],
+//         qty: [
+//             'qty1',
+//             'qty2',
+//             'qty3'
+//         ]
+//     },
+//     directions: [
+//         'direction1',
+//         'direction2',
+//         'direction3'
+//     ]
+// }
